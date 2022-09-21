3d90bab1178ece787e0d47f8d81cee260f45c5c8
Vinicius Gabriel <viniig1709@gmail.com>
2022-09-21 00:20:26 -0300

    Tela de rotulagem

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,53 @@
+import * as React from 'react'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+// import reactLogo from './assets/react.svg'
+import './App.sass'
+
+import Button from '@mui/material/Button'
+// import DeleteIcon from '@mui/icons-material/Delete'
+import SendIcon from '@mui/icons-material/Send'
+import CancelIcon from '@mui/icons-material/Cancel'
+import Woman2Icon from '@mui/icons-material/Woman2'
+import LocalPoliceIcon from '@mui/icons-material/LocalPolice'
+import PanToolIcon from '@mui/icons-material/PanTool'
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff'
+import MoodBadIcon from '@mui/icons-material/MoodBad'
+import TwitterIcon from '@mui/icons-material/Twitter';
+
+// import Stack from '@mui/material/Stack'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main>
+      <div className="box-tweet">
+        <div className="titulo">
+          <h2>Rotule os <span className="cor-tweet"> Tweets </span> <TwitterIcon className="cor-logo-twitter"/> dizendo se estão relacionados com crimes ou não!</h2>
+        </div>
+        <div className="conteudo">
+          <h3>Tweet</h3>
+          <div className="opcoes-rotulo">
+            <Button variant="outlined" startIcon={<CancelIcon />} className="espaco-botoes">
+              Não possui relação com crimes
+            </Button>
+            <div className="categorias-crimes">
+              <Button variant="contained" endIcon={<Woman2Icon />} className="espaco-botoes">
+                Assédio
+              </Button>
+              <Button variant="contained" endIcon={<PanToolIcon />} className="espaco-botoes">
+                Roubo
+              </Button>
+              <Button variant="contained" endIcon={<VisibilityOffIcon />} className="espaco-botoes">
+                Furto
+              </Button>
+              <Button variant="contained" endIcon={<MoodBadIcon />} className="espaco-botoes">
+                Homicídio
+              </Button>
+            </div>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    </main>
   )
 }
 
