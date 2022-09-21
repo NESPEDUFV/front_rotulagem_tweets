54d6880602ab3d4b3f22222dce66201aecc296af
Vinicius Gabriel <viniig1709@gmail.com>
2022-09-21 00:21:21 -0300

    Tela de rotulagem (2)

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,63 @@
 import * as React from 'react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 // import reactLogo from './assets/react.svg'
 import './App.sass'
 
 import Button from '@mui/material/Button'
-// import DeleteIcon from '@mui/icons-material/Delete'
-import SendIcon from '@mui/icons-material/Send'
+
 import CancelIcon from '@mui/icons-material/Cancel'
 import Woman2Icon from '@mui/icons-material/Woman2'
-import LocalPoliceIcon from '@mui/icons-material/LocalPolice'
+// import LocalPoliceIcon from '@mui/icons-material/LocalPolice'
 import PanToolIcon from '@mui/icons-material/PanTool'
 import VisibilityOffIcon from '@mui/icons-material/VisibilityOff'
 import MoodBadIcon from '@mui/icons-material/MoodBad'
-import TwitterIcon from '@mui/icons-material/Twitter';
+import TwitterIcon from '@mui/icons-material/Twitter'
 
-// import Stack from '@mui/material/Stack'
+const API_URL = "http://localhost:3000/"
 
 function App() {
+
+  const [tweet, setTweet] = useState('')
+
+  const carregarTweetRotular = async () => {
+    const response = await fetch(`${API_URL}tweets/listar/tweet/rotular`, {
+      method: "GET"
+    })
+
+    const data = await response.json()
+
+    // console.log(data)
+
+    if (!data.error)
+      setTweet(data.tweet)
+    
+  }
+
+  const rotulaTweet = async (categoria) => {
+    console.log(categoria, tweet.idTweet)
+    console.log(JSON.stringify({ categoriaRotulo: categoria, idTweet: tweet.idTweet }))
+    const response = await fetch(`${API_URL}tweets/rotular/tweet`, {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+        // 'Access-Control-Request-Headers': 'Content-Type, Authorization'
+      },
+      method: "POST",
+      body: JSON.stringify({ categoriaRotulo: categoria, idTweet: tweet.idTweet })
+    })
+
+    const data = await response.json()
+
+    // console.log(data, categoria)
+
+    if (!data.error)
+      carregarTweetRotular()
+
+  }
+
+  useEffect(() => {
+    carregarTweetRotular()
+  }, [])
 
   return (
     <main>
@@ -25,22 +66,47 @@ function App() {
           <h2>Rotule os <span className="cor-tweet"> Tweets </span> <TwitterIcon className="cor-logo-twitter"/> dizendo se estão relacionados com crimes ou não!</h2>
         </div>
         <div className="conteudo">
-          <h3>Tweet</h3>
+          <h3>{tweet.text}</h3>
           <div className="opcoes-rotulo">
-            <Button variant="outlined" startIcon={<CancelIcon />} className="espaco-botoes">
+            <Button
+              variant="outlined"
+              startIcon={<CancelIcon />}
+              className="espaco-botoes"
+              onClick={() => rotulaTweet('Sem relação')}
+            >
               Não possui relação com crimes
             </Button>
             <div className="categorias-crimes">
-              <Button variant="contained" endIcon={<Woman2Icon />} className="espaco-botoes">
+              <Button
+                variant="contained"
+                endIcon={<Woman2Icon />}
+                className="espaco-botoes"
+                onClick={() => rotulaTweet('Assédio')}
+              >
                 Assédio
               </Button>
-              <Button variant="contained" endIcon={<PanToolIcon />} className="espaco-botoes">
+              <Button
+                variant="contained"
+                endIcon={<PanToolIcon />}
+                className="espaco-botoes"
+                onClick={() => rotulaTweet('Roubo')}
+              >
                 Roubo
               </Button>
-              <Button variant="contained" endIcon={<VisibilityOffIcon />} className="espaco-botoes">
+              <Button
+                variant="contained"
+                endIcon={<VisibilityOffIcon />}
+                className="espaco-botoes"
+                onClick={() => rotulaTweet('Furto')}
+              >
                 Furto
               </Button>
-              <Button variant="contained" endIcon={<MoodBadIcon />} className="espaco-botoes">
+              <Button
+                variant="contained"
+                endIcon={<MoodBadIcon />}
+                className="espaco-botoes"
+                onClick={() => rotulaTweet('Homicídio')}
+              >
                 Homicídio
               </Button>
             </div>
